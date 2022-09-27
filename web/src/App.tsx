e6ba28fcbbd45345a8dd01d4a6494fcb3fbf8352
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-09-27 13:42:36 -0300

    (web)feat: Listagem dos jogos em tela

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,8 +4,32 @@ import logoImg from './assets/logo.svg';
 
 import './styles/main.css';
 import { CreateAdBanner } from './components/CreateAdBanner';
+import { useEffect, useState } from 'react';
+
+interface IGame {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
 
 export const App = () => {
+  const [games, setGames] = useState<IGame[]>([]);
+
+  const fetchData = async () => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  {
+    console.log(games);
+  }
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="Logomarca do NLW eSports" />
@@ -19,11 +43,16 @@ export const App = () => {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-20">
-        <GameBanner
-          bannerUrl="/game-1.png"
-          title="League of Legends"
-          adsCount={5}
-        />
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
       </div>
 
       <CreateAdBanner />
